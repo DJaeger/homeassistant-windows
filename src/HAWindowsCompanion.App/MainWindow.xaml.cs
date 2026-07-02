b341778f4df2e693572265ed19cf341edc1a18f3
Daniel Jäger <git@djaeger.info>
2026-07-02 13:08:40 +0200

    Fix MinWidth/MinHeight not supported on Window in WinUI 3
    Implement native WinUI 3 compatible enforcing of MinWidth/MinHeight

--- src/HAWindowsCompanion.App/MainWindow.xaml.cs
+++ src/HAWindowsCompanion.App/MainWindow.xaml.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Runtime.InteropServices;
 using System.Windows.Input;
 using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
+using WinRT.Interop;
 using HAWindowsCompanion.App.Views;
 
 namespace HAWindowsCompanion.App;
@@ -14,6 +16,13 @@ namespace HAWindowsCompanion.App;
 /// </summary>
 public sealed partial class MainWindow : Window
 {
+    private const int MinWidth = 480;
+    private const int MinHeight = 640;
+
+    private IntPtr _hwnd;
+    private IntPtr _oldWndProc;
+    private WndProc? _newWndProc;
+
     public ICommand ShowWindowCommand { get; }
     public ICommand OpenSettingsCommand { get; }
     public ICommand QuitCommand { get; }
@@ -29,11 +38,22 @@ public sealed partial class MainWindow : Window
         Title = "Home Assistant Companion";
         ExtendsContentIntoTitleBar = true;
 
+        // Configure window presenter and maximize on startup
+        if (AppWindow.Presenter is OverlappedPresenter presenter)
+        {
+            presenter.IsResizable = true;
+            presenter.IsMinimizable = true;
+        }
+
         // Minimize to tray on close instead of exiting
         AppWindow.Closing += OnClosing;
 
-        // Navigate to main page by default
-        ContentFrame.Navigate(typeof(MainPage));
+        // Install Win32 window procedure to enforce minimum window size
+        _hwnd = WindowNative.GetWindowHandle(this);
+        _newWndProc = new WndProc(WindowProc);
+        _oldWndProc = SetWindowLongPtr(_hwnd, GWLP_WNDPROC, Marshal.GetFunctionPointerForDelegate(_newWndProc));
+
+        // Navigation will be handled by App.xaml.cs based on configuration state
     }
 
     private void OnClosing(AppWindow sender, AppWindowClosingEventArgs args)
@@ -66,4 +86,67 @@ public sealed partial class MainWindow : Window
         Close();
         Application.Current.Exit();
     }
+
+    private IntPtr WindowProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam)
+    {
+        if (msg == WM_GETMINMAXINFO)
+        {
+            // Get DPI scaling factor
+            uint dpi = GetDpiForWindow(hWnd);
+            double scaleFactor = dpi / 96.0; // 96 DPI is 100% scaling
+
+            // Marshal MINMAXINFO structure from lParam
+            MINMAXINFO minMaxInfo = Marshal.PtrToStructure<MINMAXINFO>(lParam);
+
+            // Set minimum tracking size with DPI scaling
+            minMaxInfo.ptMinTrackSize.X = (int)(MinWidth * scaleFactor);
+            minMaxInfo.ptMinTrackSize.Y = (int)(MinHeight * scaleFactor);
+
+            // Marshal back to lParam
+            Marshal.StructureToPtr(minMaxInfo, lParam, true);
+
+            return IntPtr.Zero;
+        }
+
+        // Call original window procedure for all other messages
+        return CallWindowProc(_oldWndProc, hWnd, msg, wParam, lParam);
+    }
+
+    #region Win32 Interop
+
+    private delegate IntPtr WndProc(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct POINT
+    {
+        public int X;
+        public int Y;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct MINMAXINFO
+    {
+        public POINT ptReserved;
+        public POINT ptMaxSize;
+        public POINT ptMaxPosition;
+        public POINT ptMinTrackSize;
+        public POINT ptMaxTrackSize;
+    }
+
+    private const uint WM_GETMINMAXINFO = 0x0024;
+    private const int GWLP_WNDPROC = -4;
+
+    [DllImport("user32.dll", EntryPoint = "GetWindowLongPtr")]
+    private static extern IntPtr GetWindowLongPtr(IntPtr hWnd, int nIndex);
+
+    [DllImport("user32.dll", EntryPoint = "SetWindowLongPtr")]
+    private static extern IntPtr SetWindowLongPtr(IntPtr hWnd, int nIndex, IntPtr dwNewLong);
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr CallWindowProc(IntPtr lpPrevWndFunc, IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+    [DllImport("user32.dll")]
+    private static extern uint GetDpiForWindow(IntPtr hWnd);
+
+    #endregion
 }
