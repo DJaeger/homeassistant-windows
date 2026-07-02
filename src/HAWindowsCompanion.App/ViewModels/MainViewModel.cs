407a7953e3575adae38f97a11cde5a4d571e2f4c
Daniel Jäger <git@djaeger.info>
2026-07-02 20:01:28 +0200

    Add Settings button to MainPage

--- src/HAWindowsCompanion.App/ViewModels/MainViewModel.cs
+++ src/HAWindowsCompanion.App/ViewModels/MainViewModel.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using HAWindowsCompanion.App.Services;
+using HAWindowsCompanion.App.Views;
 using HAWindowsCompanion.Core.Interfaces;
 using HAWindowsCompanion.Core.Models;
 
@@ -10,16 +14,22 @@ public sealed partial class MainViewModel : ObservableObject
 {
     private readonly IEnumerable<ISensorProvider> _sensors;
     private readonly ICredentialStore _credentialStore;
+    private readonly NavigationService _navigationService;
 
     [ObservableProperty] private string _connectionStatus = "Disconnected";
     [ObservableProperty] private string _serverUrl = "Not configured";
 
     public ObservableCollection<SensorInfo> ActiveSensors { get; } = new();
+    public ICommand OpenSettingsCommand { get; }
 
-    public MainViewModel(IEnumerable<ISensorProvider> sensors, ICredentialStore credentialStore)
+    public MainViewModel(IEnumerable<ISensorProvider> sensors, ICredentialStore credentialStore, NavigationService navigationService)
     {
         _sensors = sensors;
         _credentialStore = credentialStore;
+        _navigationService = navigationService;
+
+        OpenSettingsCommand = new RelayCommand(() => _navigationService.Navigate(typeof(SettingsPage)));
+
         LoadStatus();
     }
 
